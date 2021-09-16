eaa59c04fa80c43946491e221f6f1e10928559df
Riccarpa <riccardopagni99@gmail.com>
2021-09-16 12:49:44 +0200

    slider 2

--- don-pepe/src/App.vue
+++ don-pepe/src/App.vue
@@ -1,51 +1,58 @@
 <template>
   <div id="app">
-   <Header/>
-   <main>
-     <Slider :images="tasteSliderImg" />
-     <ImgSection/>
-   </main>
+    <Header />
+    <main>
+      <Slider :images="tasteSliderImg" />
+      <ImgSection />
+      <Slider :citazioni="testimonialSlider" />
+    </main>
   </div>
 </template>
 
 <script>
-
-import Header from '@/components/Header.vue';
-import Slider from '@/components/Slider.vue';
-import ImgSection from '@/components/ImgSection.vue';
+import Header from "@/components/Header.vue";
+import Slider from "@/components/Slider.vue";
+import ImgSection from "@/components/ImgSection.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Slider,
-    ImgSection
+    ImgSection,
   },
-  data(){
-    return{
-      tasteSliderImg:[
+  data() {
+    return {
+      tasteSliderImg: [
         {
-          url:require('@/img/h3-rev-img-2.png'),
-          bgUrl:require('@/img/h3-rev-img-1.png'),
-          id:0
+          url: require("@/img/h3-rev-img-2.png"),
+          bgUrl: require("@/img/h3-rev-img-1.png"),
+          id: 0,
         },
         {
-          url:require('@/img/h3-rev-img-4.png'),
-          bgUrl:require('@/img/h3-rev-img-3.png'),
-          id:1
+          url: require("@/img/h3-rev-img-4.png"),
+          bgUrl: require("@/img/h3-rev-img-3.png"),
+          id: 1,
         },
         {
-          url:require('@/img/h3-rev-img-6.png'),
-          bgUrl:require('@/img/h3-rev-img-5.png'),
-          id:2
+          url: require("@/img/h3-rev-img-6.png"),
+          bgUrl: require("@/img/h3-rev-img-5.png"),
+          id: 2,
         },
       ],
-    }
-  }
-}
+      testimonialSlider: [
+        {
+          text:
+            "forget the trendy pizza shops, this hidden spot makes the best new york-style pizza slice in naples",
+          bgUrl: require("@/img/h3-testimonials-bckgrnd.jpg"),
+          id: 0,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-  @import 'bootstrap';
-  @import './scss/_style.scss';
- 
+@import "bootstrap";
+@import "./scss/_style.scss";
 </style>
--- don-pepe/src/components/Slider.vue
+++ don-pepe/src/components/Slider.vue
@@ -1,92 +1,148 @@
 <template>
   <section>
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="h-100 position-relative">
-            <div class="carousel-inner h-100">
-                <div v-for="img in images" :key="img.id"  class="carousel-item " :class="img.id==0 ? 'active' : ''">
-                    <div class="img-center position-relative">
-                        <img class="bg" :src="img.bgUrl" alt="">
-                        <img :src="img.url" class="d-block " alt="...">
-                    </div>
-                </div>
+    <div
+      id="carouselExampleControls"
+      class="carousel slide "
+      :class="citazioni ? 'bg2 mt-2' : 'bg1'"
+      data-bs-ride="carousel"
+    >
+      <div class="h-100 position-relative">
+        <div class="carousel-inner h-100">
+          <!-- slider 1 images -->
+          <div
+            v-for="img in images"
+            :key="img.id"
+            class="carousel-item "
+            :class="img.id == 0 ? 'active' : ''"
+          >
+            <div class="img-center position-relative">
+              <img class="bg" :src="img.bgUrl" alt="" />
+              <img :src="img.url" class="d-block " alt="..." />
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <div class="carousel-control-prev" aria-hidden="true"><span class="prev button">PREV</span></div>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <div class="carousel-control-next" aria-hidden="true"><span class="next button">NEXT</span></div>
-                <span class="visually-hidden">Next</span>
-            </button>
+          </div>
+          <!-- slider 2  citazioni -->
+          <div
+            v-for="item in citazioni"
+            :key="item.id"
+            class="carousel-item "
+            :class="item.id == 0 ? 'active' : ''"
+          >
+            <div class="img-center position-relative">
+              <img class="bg" :src="item.bgUrl" alt="" />
+              <div class="cit">
+                <i
+                  class="fas fa-quote-right d-block mb-4 fa-2x text-danger"
+                ></i>
+                <span class="text">{{ item.text }}</span>
+                <span class="text-danger d-block m-2"
+                  >WASHINGTON POST 2018
+                </span>
+                <i class="fas fa-ellipsis-h fa-2x text-danger"></i>
+              </div>
+            </div>
+          </div>
         </div>
-       
-     </div> 
-     
+        <!-- buttons -->
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <div class="carousel-control-prev" aria-hidden="true">
+            <span class="prev button">PREV</span>
+          </div>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <div class="carousel-control-next" aria-hidden="true">
+            <span class="next button">NEXT</span>
+          </div>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-
 export default {
-name:'Slider',
-props:['images'],
-
-}
-
-
+  name: "Slider",
+  props: ["images", "citazioni"],
+};
 </script>
 
 <style lang="scss" scoped>
+@import "../scss/_style.scss";
 
-    @import '../scss/_style.scss';
-    section{
-        overflow: hidden;
+// slider images
+.bg1 {
+  background-color: $head-bg;
+  height: 400px;
+}
+// generics
+section {
+  overflow: hidden;
+}
+.carousel-item {
+  height: 100%;
+
+  .img-center {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      height: 300px;
+      z-index: 1;
     }
-    .carousel{
-        height: 400px;
-        background-color: $head-bg;   
+    .bg {
+      position: absolute;
+      z-index: 0;
     }
-    .carousel-item{
-        height: 100%;
-        
-        
-        .img-center{
-            height: 100%;
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-           img{
-               height: 300px;
-               z-index: 1;
-           }
-           .bg{
-               position: absolute;
-               z-index: 0;
-           }
-        }
+  }
+}
+.carousel-control-next,
+.carousel-control-prev {
+  opacity: 1;
+  .button {
+    position: absolute;
+    background-color: white;
+    padding: 10px 10px 20px 10px;
+    color: $button-text;
+    border-radius: 50%;
+    border-radius: 50%;
+  }
+  .next {
+    transform: rotate(270deg);
+    right: -50%;
+    top: 35%;
+  }
+  .prev {
+    transform: rotate(90deg);
+    left: -50%;
+    top: 35%;
+  }
+}
+// slider citazioni
+.bg2 {
+  background-color: #f6f7f2;
+  height: 300px;
+  .cit {
+    z-index: 1;
+    text-transform: uppercase;
+    width: 600px;
+    text-align: center;
+    .text {
+      font-weight: 600;
+      font-size: 20px;
     }
-    .carousel-control-next, 
-    .carousel-control-prev{
-        opacity: 1;
-        .button{
-           position: absolute;
-           background-color: white;
-           padding: 10px 10px 20px 10px;
-           color: $button-text;
-           border-radius: 50%;
-           border-radius: 50%;
-        }
-        .next{
-          transform: rotate(270deg);
-          right: -50%;
-          top: 35%;
-        }
-        .prev{
-          transform: rotate(90deg);
-          left: -50%;
-          top: 35%;
-        }
-    }
-    
+  }
+}
 </style>
